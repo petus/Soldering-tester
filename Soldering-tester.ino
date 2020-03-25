c428a8954de3508d8baf8b1eff97e3b2d6474e4d
petus <petus@users.noreply.github.com>
2020-03-25 20:30:55 +0100

    Create Soldering-tester.ino

---
+++ Soldering-tester.ino
@@ -0,0 +1,123 @@
+/*
+ * SOLDERING TESTER v0.1
+ * 
+ * made by chiptron.cz
+ * 
+ * Probe Vcc of Nucleo board with 100k resistor to GND of tested board
+ * Probe inputs (A0, A1,...) to GPIO of tested board
+ * 
+ * Push the user button for the soldering test
+ * If the LED is blinking, some analog channel is badly soldered, otherwise it is ok
+ * 
+ */
+
+#define A0 PA0
+#define A1 PA1
+#define A2 PA4 
+#define A3 PB0 
+#define A4 PC1 
+#define A5 PC0 
+#define A6 PC2 
+#define A7 PC3
+
+#define UB PC13 // user button
+#define SL PA5  // status LED
+
+#define ADCformula (1000 * (3.3 / 1023.0))
+
+uint8_t analogArray[] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+float anaVal = 0;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(UB, INPUT); // user button
+  pinMode(SL, OUTPUT);  //status LED
+  
+  digitalWrite(SL, LOW);
+}
+
+void loop() {
+
+  bool error = false;
+  digitalWrite(SL, HIGH);
+  
+  if(!(digitalRead(UB)))
+  {
+    Serial.println("Start of testing");
+
+    for(uint8_t i=0; i <= 7; i++)
+    {
+      anaVal = 0;
+      switch (i)
+      {
+        case 0:
+          anaVal = analogRead(A0) * ADCformula;
+          break;
+
+        case 1:
+          anaVal = analogRead(A1) * ADCformula;
+          break;
+
+        case 2:
+          anaVal = analogRead(A2) * ADCformula;
+          break;
+
+        case 3:
+          anaVal = analogRead(A3) * ADCformula;
+          break;
+
+        case 4:
+          anaVal = analogRead(A4) * ADCformula;
+          break;
+
+        case 5:
+          anaVal = analogRead(A5) * ADCformula;
+          break;
+
+        case 6:
+          anaVal = analogRead(A6) * ADCformula;
+          break;
+
+        case 7:
+          anaVal = analogRead(A7) * ADCformula;
+          break;
+
+        default:
+          break;
+      }
+
+      if((anaVal >= 300) && (anaVal <= 800))
+      {
+        Serial.print("OK: channel ");
+        Serial.print(i);
+        Serial.print("; ");
+        Serial.print(anaVal);
+        Serial.println(" mV");
+      }
+      else 
+      {
+        Serial.print("Badly soldered: channel ");
+        Serial.print(i);
+        Serial.print("; ");
+        Serial.print(anaVal);
+        Serial.println(" mV");
+        error = true;
+      }
+    }
+  }
+
+  if(error)
+  {
+    for(uint8_t j=0; j<=10; j++)
+    {
+      digitalWrite(SL, LOW);
+      delay(100);
+      digitalWrite(SL, HIGH);
+      delay(100);
+    }
+  }
+  delay(500);
+}
